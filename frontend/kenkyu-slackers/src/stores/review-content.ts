7a28739e63088ad199f6f6d3c6899b5d34db00a7
Malb7mm <ykmeak9g@gmail.com>
2025-09-10 09:55:16 +0900

    fix: ストアの初期値でaspectsに変なIDが入ってしまっていたので修正

--- frontend/kenkyu-slackers/src/stores/review-content.ts
+++ frontend/kenkyu-slackers/src/stores/review-content.ts
@@ -1,10 +1,14 @@
 import { defineStore } from 'pinia';
 
 export const useReviewContentStore = defineStore("markdown-content", {
-  state: () => ({
+  state: (): {
+    markdownContent: string,
+    industryIds: string[],
+    importantAspects: string[],
+  } => ({
     markdownContent: "",
-    industryIds: [""],
-    importantAspects: [""],
+    industryIds: [],
+    importantAspects: [],
   }),
   getters: {
     singleIndustryId: (state) => state.industryIds[0]
